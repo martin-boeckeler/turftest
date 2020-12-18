e1c8f569ec91d595e95488a2ea1dee2833b5cddb
Martin Boeckeler <martin.boeckeler@gmail.com>
2020-12-18 10:24:08 +0100

    add center of median

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import featureCollection from './featureCollection.json';
+import {centerMedian} from '@turf/turf';
+
 
 function App() {
+  const center = centerMedian(featureCollection);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          center: {JSON.stringify(center)}
         </p>
         <a
           className="App-link"
